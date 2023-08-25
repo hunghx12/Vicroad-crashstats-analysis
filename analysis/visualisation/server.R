e2a24f4904eab0afc0b0ecb4437278d8c330dd8a
Xuan Hung Ho <hung12hx@gmail.com>
2023-08-26 05:46:50 +1000

    Initialise visualisation project, plot clusted points for accident locations

---
+++ analysis/visualisation/server.R
@@ -0,0 +1,28 @@
+#
+# This is the server logic of a Shiny web application. You can run the
+# application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(leaflet)
+library(maps)
+library(dplyr)
+
+nodes = read.csv('../../data/clean/NODE.csv')
+
+function(input, output, session) {
+  output$melbourneMap <- renderLeaflet({
+    leaflet(data = nodes) %>%
+      addProviderTiles("CartoDB.Positron") %>%
+      setView(lat = -36.9848, lng = 143.3906, zoom = 6.2) %>%
+      addCircleMarkers(lng = ~Long, lat = ~Lat, radius = 0.1, 
+                       clusterOptions = markerClusterOptions(
+                         showCoverageOnHover = FALSE
+                       ))
+  })
+  
+}
+
---
+++ analysis/visualisation/ui.R
@@ -0,0 +1,20 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(leaflet)
+
+fluidPage(
+  tags$head(tags$style(
+    HTML('
+             #suburb_info {background-color: rgba(255,255,255,1); margin: 100;}
+         ')
+  )),
+  titlePanel("Crash Stats"),
+  leafletOutput("melbourneMap", height = 600)
+)
